71009f35544c238dcc69fa349493a09b5621ea57
Tommy Calvy <42327902+tommycalvy@users.noreply.github.com>
2023-12-28 23:53:24 -0600

    Initiall commit

---
+++ after/plugin/telescope.lua
@@ -0,0 +1,23 @@
+local builtin = require('telescope.builtin')
+
+vim.keymap.set('n', '<leader>pf', builtin.find_files, {})
+vim.keymap.set('n', '<C-p>', builtin.git_files, {})
+vim.keymap.set('n', '<leader>ps', function()
+	builtin.grep_string({ search = vim.fn.input("Grep > ") });
+end)
+
+-- require('telescope').setup{
+-- 	defaults = {
+-- 		-- Default configuration for telescope goes here:
+-- 		-- config_key = value,
+-- 		mappings = {
+-- 			i = {
+-- 				-- map actions.which_key to <C-h> (default: <C-/>)
+-- 				-- actions.which_key shows the mappings for your picker,
+-- 				-- e.g. git_{create, delete, ...}_branch for the git_branches picker
+-- 				["<C-j>"] = "move_selection_next",
+-- 				["<C-k>"] = "move_selection_previous",
+-- 			}
+-- 		}
+-- 	},
+-- }
